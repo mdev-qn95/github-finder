b31eb63774718ec77475e774ff6911a92b4d8d44
unknown <thaivanphu.qn95@gmail.com>
2021-04-13 21:42:54 +0700

    stateless functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github mr-05'
-    }
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1><i className={this.props.icon} />{this.props.title}</h1>
-            </nav>
-        )
-    }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
 
-export default Navbar
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github mr-05',
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          className='round-img'
-          style={{ width: '80px' }}
-        ></img>
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '80px' }}
+      ></img>
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
